bc967b5f80c4270ebc58aa30f1f11514a592b33b
isoneariyanto <iswanariyanto07@gmail.com>
2024-10-17 21:55:33 +0800

    routing page

--- src/components/ThatOpen.jsx
+++ src/components/ThatOpen.jsx
@@ -6,26 +6,26 @@ import { useEffect, useState } from "react";
 
 export default function ThatOpen({}) {
   const [isRender, setIsRender] = useState(false);
+  const [getFile, setGetFile] = useState(false);
 
   const components = new OBC.Components();
   const fragments = components.get(OBC.FragmentsManager);
-  const worlds = components.get(OBC.Worlds);
   const fragmentIfcLoader = components.get(OBC.IfcLoader);
-
-  const grids = components.get(OBC.Grids);
+  const worlds = components.get(OBC.Worlds);
   const world = worlds.create();
-  components.init();
-
-  async function loadIfc(e) {
+  async function loadWorld() {
     const container = document.getElementById("canvas-container");
     world.scene = new OBC.SimpleScene(components);
     world.renderer = new OBC.SimpleRenderer(components, container);
     world.camera = new OBC.SimpleCamera(components);
+    components.init();
+
     world.camera.controls.setLookAt(12, 6, 8, 0, 0, -10);
     world.scene.setup();
 
-    world.scene.three.background = null;
+    const grids = components.get(OBC.Grids);
     grids.create(world);
+    world.scene.three.background = "";
 
     await fragmentIfcLoader.setup();
     const excludedCats = [
@@ -38,7 +38,9 @@ export default function ThatOpen({}) {
       fragmentIfcLoader.settings.excludedCategories.add(cat);
     }
     fragmentIfcLoader.settings.webIfc.COORDINATE_TO_ORIGIN = true;
+  }
 
+  async function loadIfc(e) {
     const file = e.target.files[0];
     const data = await file.arrayBuffer();
     const buffer = new Uint8Array(data);
@@ -51,25 +53,24 @@ export default function ThatOpen({}) {
     fragments.dispose();
   }
 
-  // useEffect(() => {
-  //   console.log(isRender);
-  //   if (!isRender) fragmentBbox.reset();
-  //   else get();
-  // }, [isRender]);
-
   return (
-    <>
-      <div className="flex flex-col items-center gap-2">
-        <input
-          type="file"
-          className="file-input file-input-bordered w-full max-w-xs mt-2"
-          onChange={loadIfc}
-        />
-        <button className="btn w-fit" onClick={disposeFragments}>
-          Dispose
-        </button>
+    <div className="flex p-6">
+      <div className="flex flex-col items-center gap-4 justify-center h-screen w-4/12">
+        <div className="flex flex-col items-center gap-4 justify-center w-fit p-4 rounded-md shadow border">
+          <input
+            type="file"
+            className="file-input file-input-bordered w-full max-w-xs mt-2"
+            onChange={loadIfc}
+          />
+          <button className="btn w-fit" onClick={loadWorld}>
+            Load World
+          </button>
+          <button className="btn w-fit" onClick={disposeFragments}>
+            Dispose
+          </button>
+        </div>
       </div>
-      <div id="canvas-container"></div>
-    </>
+      <div id="canvas-container" className="w-8/12 h-fit"></div>
+    </div>
   );
 }
